23a2225e6b1525ae9773e5bd7eb7f61609aee9c4
Llevella <irinavkhomutova@gmail.com>
2021-01-29 19:03:51 +0700

    additional functions were added

--- include/mmio.h
+++ include/mmio.h
@@ -4,7 +4,11 @@
 #include <map>
 #include <iostream>
 #include <fstream>
+#include <functional>
+#include <algorithm>
+#include <cctype>
 
+#include "mmio_functions.h"
 #include "mmio_exceptions.h"
 #include "mmio_header.h"
 #include "mmio_matrix.h"
